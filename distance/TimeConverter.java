d82cf16a3c876bd0124e166ca281250461280512
Shreevathsatantry <146634562+Shreevathsatantry@users.noreply.github.com>
2023-11-23 18:28:41 +0530

    Add files via upload

---
+++ distance/TimeConverter.java
@@ -0,0 +1,13 @@
+// time/TimeConverter.java
+
+package time;
+
+public class TimeConverter {
+    public static int hoursToMinutes(int hours) {
+        return hours * 60;
+    }
+
+    public static int hoursToSeconds(int hours) {
+        return hours * 3600;
+    }
+}
